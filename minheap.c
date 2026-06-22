274c0661d8600e3e8d91728f4abee9a95c880f37
Gabriel Guerra <gabrielescorelguerra@gmail.com>
2026-06-22 14:11:18 -0300

    adiciona arquivo minheap.c

---
+++ minheap.c
@@ -0,0 +1,249 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct No
+{
+    float x;
+    float y;
+
+    int pos_heap;
+    float erro;
+
+    int altura;
+    struct No *pai, *esq, *dir;
+    struct No *ant, *prox;
+}No;
+
+
+typedef struct heap{
+    int tam;
+    int capacidade;
+    struct No **no;
+}heap;
+
+
+No* no_criar(float x_ponto, float y_ponto) {
+    No *no;
+    if (! (no = malloc (sizeof (No))))
+        return NULL;
+
+    no->x = x_ponto;
+    no->y = y_ponto;
+
+    no->altura = 0;
+    no->pos_heap = -1;
+
+    no->pai = NULL;
+    no->esq = NULL;
+    no->dir = NULL;
+
+    no->prox = NULL;
+    no->ant = NULL;
+
+    return no;
+}
+
+int HeapVazio(heap *h) {
+    if(h == NULL || h->tam < 1)
+        return 1;
+    return 0;
+}
+
+heap *cria_heap(int capacidade){
+    heap *h = malloc(sizeof(struct heap));
+    if (h == NULL)
+        return NULL;
+    h->tam = 0;
+    h->capacidade = capacidade;
+    h->no = malloc((capacidade + 1) * sizeof(No *));
+    return h;
+}
+
+int InsereHeap(heap *h, No *n){
+    int i;
+    struct No *aux;
+
+    i = h->tam + 1;
+    if (i > h->capacidade)
+        return 0;
+
+    h->no[i] = n;
+    h->no[i]->pos_heap = i;
+    h->tam++;
+
+    while(i > 1 && h->no[i/2]->erro > h->no[i]->erro) {
+        aux = h->no[i];
+        h->no[i] = h->no[i/2];
+        h->no[i/2] = aux;
+        h->no[i]->pos_heap = i;
+        h->no[i/2]->pos_heap = i/2;
+        i = i / 2;
+    }
+    return 1; 
+}
+
+void DesceNoHeap(heap *h, int pai) {
+    int menor = pai;
+    int esq = 2 * pai;
+    int dir = 2 * pai + 1;
+    No *aux;
+
+    if (esq <= h->tam && h->no[esq]->erro < h->no[menor]->erro) {
+        menor = esq;
+    }
+    if (dir <= h->tam && h->no[dir]->erro < h->no[menor]->erro) {
+        menor = dir;
+    }
+
+    if (menor != pai) {
+        aux = h->no[pai];
+        h->no[pai] = h->no[menor];
+        h->no[menor] = aux;
+
+        h->no[pai]->pos_heap = pai;
+        h->no[menor]->pos_heap = menor;
+
+        DesceNoHeap(h, menor);
+    }
+}
+
+void SobeNoHeap(heap *h, int i){
+    No *aux;
+    while(i > 1 && h->no[i/2]->erro > h->no[i]->erro) {
+        aux = h->no[i];
+        h->no[i] = h->no[i/2];
+        h->no[i/2] = aux;
+        h->no[i]->pos_heap = i;
+        h->no[i/2]->pos_heap = i/2;
+        i = i / 2;
+    }
+}
+
+void Heapify(heap *h) {
+    if (h == NULL || h->tam <= 1) 
+        return;
+
+    for (int i = h->tam / 2; i >= 1; i--) {
+        DesceNoHeap(h, i);
+    }
+}
+
+No *HeapMinino(heap *h){
+    if (HeapVazio(h))
+        return NULL;
+    return h->no[1];
+}
+
+No *RemoveHeap(heap *h){
+    No *n;
+    if (HeapVazio(h)){
+        return NULL;
+    }
+
+    n = h->no[1];
+    if (h->tam == 1){
+        h->tam--;
+        return n;
+    }
+
+    h->no[1] = h->no[h->tam];
+    h->no[1]->pos_heap = 1;
+    h->tam--;
+    DesceNoHeap(h, 1);
+    return n;
+}
+
+void AtualizaHeap(heap *h, int pos_heap, int novo_erro){
+    if (h == NULL || h->tam < 1 || pos_heap < 1 || pos_heap > h->tam)
+        return;
+    float erro_antes = h->no[pos_heap]->erro;
+    h->no[pos_heap]->erro = novo_erro;
+
+    if (erro_antes < novo_erro){
+        DesceNoHeap(h, pos_heap);
+        return;
+    }
+    else if (erro_antes > novo_erro){
+        SobeNoHeap(h, pos_heap);
+        return;
+    }
+}
+
+void ImprimeHeap(heap *h) {
+    int i;
+
+    if(HeapVazio(h))
+        printf("Fila vazia");
+    printf("capacidade: %d \n", h->capacidade);
+    printf("tamanho: %d \n", h->tam);
+    for(i=1; i<=h->tam; i++)
+        printf("(%f %f) %f, %d\n", h->no[i]->x, h->no[i]->y, h->no[i]->erro, h->no[i]->pos_heap);
+}
+
+/* da pra ser os 6 floats ou mandar o no e usar no->x, no->y
+float area_triangulo(ponto1, ponto2, ponto3){
+    float area = (ponto1->x * (ponto2->y - ponto3->y) + ponto2->x * (ponto3->y - ponto1->y) + ponto3->x * (ponto1->y - ponto2->y)) / 2;
+    if (area < 0)
+        area = -area;
+    return area;
+}
+
+float altura2_triangulo(p1, p2, p3){
+    float area, dx, dy;
+    area = area_triangulo(p1, p2, p3);
+    dx = p3->x - p1->x;
+    dy = p3->y - p1->y;
+    return 4 * (area * area)/(dx * dx + dy * dy);
+}
+*/
+
+int main(){
+    int w;
+    float x, y, erro;
+    printf("Digite capacidade\n");
+    scanf("%i", &w);
+    heap *h = cria_heap(w);
+    do{
+    printf("1: insere(x, y, erro)\n");
+    printf("2: remove\n");
+    printf("3: primeiro\n");
+    printf("4: mostra heap\n");
+    printf("0: encerra\n");
+    scanf("%d", &w);
+        switch (w)
+        {
+        case 1:
+            scanf("%f %f %f", &x, &y, &erro);
+            No *n = no_criar(x, y);
+            n->erro = erro;
+            InsereHeap(h, n);
+            break;
+        case 2:
+            No *s = RemoveHeap(h);
+            if (s == NULL){
+                (printf("vazio\n"));
+                break;
+            }
+            printf("Saida: (%f %f) %f\n", s->x, s->y, s->erro);
+            break;
+        case 3:
+            No *a = HeapMinino(h);
+            if (a == NULL){
+                (printf("vazio\n"));
+                break;
+            }
+            printf("Primeiro: (%f %f) %f\n", a->x, a->y, a->erro);
+            break;
+        case 4:
+            ImprimeHeap(h);
+            break;
+        case 0:
+            printf("Encerrando\n");
+            break;
+        default:
+            printf("Opção invalida\n");
+            break;
+        }
+    }while(w != 0);
+}
